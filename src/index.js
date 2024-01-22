2f33d04e804b8897bf83884f126a25b70663b346
Anastasiya Andronova <anastasiya5.88@mail.ru>
2024-01-22 22:23:35 +0300

    feat: made new build of hangman game

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './assets/js/layout';
-import './assets/js/words';
-import './assets/js/game';
-import './assets/icons/gallows.svg';
-import './assets/icons/apple.svg';
-import './assets/icons/favicon.ico';
-import './assets/fonts/Gelasio-Regular.ttf';
-import './style.scss';
